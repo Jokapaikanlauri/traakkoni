16e00285d3fccb3c34989b7b9be91bef1f79ee85
Lauri Hartikainen <103640313+Jokapaikanlauri@users.noreply.github.com>
2024-08-15 23:57:25 +0300

    .

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -69,11 +69,7 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('home.index')" :active="request()->routeIs('home.index')">
-                {{ __('Notes') }}
-            </x-responsive-nav-link>
-        </div>
+        
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
